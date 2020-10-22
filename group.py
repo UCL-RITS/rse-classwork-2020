4b5f0d4807d4797987f5e0242bd53af59072f242
Imraj <imraj.singh.20@ucl.ac.uk>
2020-10-22 11:39:11 +0100

    Added the structures

---
+++ group.py
@@ -0,0 +1,35 @@
+friends = {
+    'Jill' : {
+        "age": 26, 
+        "job": "Biologist", 
+        "relation" : {
+            "friend":"Zalika",
+            "partner":"John"
+            },
+        },
+    'Zalika' : {
+        "age": 28, 
+        "job": "Artist", 
+        "relation" : {
+            "friend":"Jill"
+            },
+        },
+    'John' : {
+        "age": 27, 
+        "job": "Writer", 
+        "relation" : {
+            "partner":"Jill"
+            },
+        },
+    'Nash' : {
+        "age": 34, 
+        "job": "Chef", 
+        "relation" : {
+            "cousin":"John",
+            "landlord":"Zalika"
+            },
+        },
+}
+
+
+print(friends)

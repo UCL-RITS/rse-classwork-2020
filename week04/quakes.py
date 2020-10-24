d3b4e5bf3b6dd00604d77174c20e5421cd7e4bac
FMAUR92 <zcemfma@ucl.ac.uk>
2020-10-24 20:04:54 +0100

    Found largest earthquakes in UK in last century

--- week04/quakes.py
+++ week04/quakes.py
@@ -1,19 +1,40 @@
 """A script to find the biggest earthquake in an online dataset."""
 
-# At the top of the file, import any libraries you will use.
-# import ...
+import requests
+import json
 
-# If you want, you can define some functions to help organise your code.
-# def helper_function(argument_1, argument_2):
-#   ...
-
-# When you run the file, it should print out the location and magnitude
-# of the biggest earthquake.
-# You can run the file with `python quakes.py` from this directory.
 if __name__ == "__main__":
-    # ...do things here to find the results...
+    # We first load the data 
+    quakes = requests.get("http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
+                          params={
+                              'starttime': "2000-01-01",
+                              "maxlatitude": "58.723",
+                              "minlatitude": "50.008",
+                              "maxlongitude": "1.67",
+                              "minlongitude": "-9.756",
+                              "minmagnitude": "1",
+                              "endtime": "2018-10-11",
+                              "orderby": "time-asc"}
+                          )
 
-    # The lines below assume that the results are stored in variables
-    # named max_magnitude and coords, but you can change that.
+    # Writing the data in a readable format
+    json_data = json.loads(quakes.text)
+
+    # We now find the relevant data and use it to search for the maximum magnitude
+    quakes_data = json_data['features']
+    magnitudes = []
+    for quake in quakes_data:
+        quake_magnitude = quake['properties']['mag']
+        magnitudes.append(quake_magnitude)
+    max_magnitude = max(magnitudes)
+    
+    # We now look for where this maximum magnitude earthquakes happened
+    coords = []
+    for quake in quakes_data:
+        if quake['properties']['mag'] == max_magnitude:
+            coord = quake['geometry']['coordinates']
+            coords.append(coord)
+            
+    # Printing the solution found
     print(f"The maximum magnitude is {max_magnitude} "
           f"and it occured at coordinates {coords}.")

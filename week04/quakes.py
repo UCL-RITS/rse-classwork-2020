e157d32c6a331cf7ab28b3394850e6f48efdd3e7
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2020-10-25 22:17:31 +0000

    Work for Issue 12, earthquake data set

--- week04/quakes.py
+++ week04/quakes.py
@@ -1,19 +1,25 @@
 """A script to find the biggest earthquake in an online dataset."""
 
-# At the top of the file, import any libraries you will use.
-# import ...
+import requests
+import json
 
-# If you want, you can define some functions to help organise your code.
-# def helper_function(argument_1, argument_2):
-#   ...
+quakes = requests.get("http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
+                      params={
+                          'starttime': "2000-01-01",
+                          "maxlatitude": "58.723",
+                          "minlatitude": "50.008",
+                          "maxlongitude": "1.67",
+                          "minlongitude": "-9.756",
+                          "minmagnitude": "1",
+                          "endtime": "2018-10-11",
+                          "orderby": "time-asc"}
+                      )
 
-# When you run the file, it should print out the location and magnitude
-# of the biggest earthquake.
-# You can run the file with `python quakes.py` from this directory.
-if __name__ == "__main__":
-    # ...do things here to find the results...
+quakes_json = json.loads(quakes.text)
+quakes_lst = quakes_json['features']
 
-    # The lines below assume that the results are stored in variables
-    # named max_magnitude and coords, but you can change that.
-    print(f"The maximum magnitude is {max_magnitude} "
-          f"and it occured at coordinates {coords}.")
+max_mag = max([ quake['properties']['mag'] for quake in quakes_lst ])
+max_mag_idx = [ idx for idx, val in enumerate([ quake['properties']['mag'] for quake in quakes_lst ]) if val == max_mag ]
+max_mag_coords = [ quakes_lst[i]['geometry']['coordinates'] for i in max_mag_idx ]
+
+print('The maximum magnitude of a quake in the UK in the past century has been a ' + str(max_mag) + '. It has occured ' + str(len(max_mag_coords)) + ' times at the coordinates ' + str(max_mag_coords))

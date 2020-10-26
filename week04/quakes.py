016a6f59bd33e057710d3188dd40b70a15cd1bfd
Andrius Vaitkus <andrius.vaitkus.16@ucl.ac.uk>
2020-10-26 16:21:14 +0000

    The maximum magnitude earthquake found and its coordinates printed

--- week04/quakes.py
+++ week04/quakes.py
@@ -2,6 +2,8 @@
 
 # At the top of the file, import any libraries you will use.
 # import ...
+import requests
+import json
 
 # If you want, you can define some functions to help organise your code.
 # def helper_function(argument_1, argument_2):
@@ -12,6 +14,32 @@
 # You can run the file with `python quakes.py` from this directory.
 if __name__ == "__main__":
     # ...do things here to find the results...
+    quakes = requests.get("http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
+                      params={
+                          'starttime': "1901-01-01",
+                          "maxlatitude": "58.723",
+                          "minlatitude": "50.008",
+                          "maxlongitude": "1.67",
+                          "minlongitude": "-9.756",
+                          "minmagnitude": "1",
+                          "endtime": "2000-12-31",
+                          "orderby": "time-asc"} # easiest way would be just to change this to "magnitude-asc"?
+                      )
+
+    quakes_json = json.loads(quakes.text) # convert text body to json format
+
+    with open('quake_data.json', 'w') as file_to_write:
+        file_to_write.write(quakes.text)
+
+    max_magnitude = 0
+    max_magnitude_index = 0
+    for i in range(len(quakes_json['features'])):
+        test_magnitude = quakes_json['features'][i]['properties']['mag']
+        if test_magnitude > max_magnitude:
+            max_magnitude = test_magnitude
+            max_magnitude_index = i
+
+    coords = quakes_json['features'][max_magnitude_index]['geometry']['coordinates']
 
     # The lines below assume that the results are stored in variables
     # named max_magnitude and coords, but you can change that.

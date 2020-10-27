e7e6103f96c35b1da41fe7f5aaf768b2b326e1a5
immyharrison <zcqsarr@ucl.ac.uk>
2020-10-27 09:35:25 +0000

    finding max mag and coordinates

--- eathquake.py
+++ eathquake.py
@@ -1,4 +1,7 @@
 import requests
+import datetime
+import json 
+
 quakes = requests.get("http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
                       params={
                           'starttime': "2000-01-01",
@@ -8,7 +11,13 @@ quakes = requests.get("http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
                           "minlongitude": "-9.756",
                           "minmagnitude": "1",
                           "endtime": "2018-10-11",
-                          "orderby": "time-asc"}    ).text
-import json 
+                          "orderby": "time-asc"}    )
+quakes_json_data = json.loads(quakes.text)
 
-json_data = json.loads(quakes.text)
+max_mag = max(quake['properties']['mag'] for quake in quakes_json_data['features'])
+
+for feature in quakes_json_data['features']:
+        if feature['properties']['mag'] == max_mag:
+            quake_coord = feature['geometry']['coordinates']
+
+print('The large magnitude earthquake was: ',max_mag, 'located at the coordinates', quake_coord)

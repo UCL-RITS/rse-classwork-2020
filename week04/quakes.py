2f68736083260a722f6f072bf2c615ea90567726
poppynikou <poppyrebecca@hotmail.co.uk>
2020-10-27 18:07:33 +0000

    Created a file which solved issue 12 in the homework

--- week04/quakes.py
+++ week04/quakes.py
@@ -1,19 +1,35 @@
 """A script to find the biggest earthquake in an online dataset."""
 
-# At the top of the file, import any libraries you will use.
-# import ...
+import requests
+import json
+import numpy as np 
 
-# If you want, you can define some functions to help organise your code.
-# def helper_function(argument_1, argument_2):
-#   ...
 
-# When you run the file, it should print out the location and magnitude
-# of the biggest earthquake.
-# You can run the file with `python quakes.py` from this directory.
 if __name__ == "__main__":
-    # ...do things here to find the results...
+    #reads in data from the internet url given 
+    quakes = requests.get("http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
+                     params={
+                          'starttime': "2000-01-01",
+                          "maxlatitude": "58.723",
+                          "minlatitude": "50.008",
+                          "maxlongitude": "1.67",
+                          "minlongitude": "-9.756",
+                          "minmagnitude": "1",
+                          "endtime": "2018-10-11",
+                          "orderby": "time-asc"})
+    #reads in data using json as a dictionary
+    earthquakes = json.loads(quakes.text)
 
-    # The lines below assume that the results are stored in variables
-    # named max_magnitude and coords, but you can change that.
-    print(f"The maximum magnitude is {max_magnitude} "
-          f"and it occured at coordinates {coords}.")
+    # creates a list of all the magnitudes of the earthquakes 
+    eq_size = [earthquakes['features'][i]['properties']['mag'] for i in range(earthquakes['metadata']['count'])]
+
+
+    # searches for the location of the largest earthquake within list eq_size
+    # location is stored as a list
+    location = [j for j in range(len(eq_size)) if eq_size[j] == np.max(eq_size)]
+
+    # gives location of the largest earthqukes
+    geog_location = earthquakes['features'][location[0]]['properties']['place']
+    
+    print(f"The maximum magnitude is {np.max(eq_size)} "
+          f"and it occured at  {geog_location}.")

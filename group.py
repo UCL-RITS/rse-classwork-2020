56663a3fed407b9f0863b921e2fac2251500c948
Jon <superuser@localhost.localdomain>
2020-10-28 13:43:13 +0000

    add read and write json

---
+++ group.py
@@ -0,0 +1,42 @@
+import json
+
+group = {
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "relations": {
+            "Zalika": "friend",
+            "John": "partner"
+        }
+    },
+    "Zalika": {
+        "age": 28,
+        "job": "artist",
+        "relations": {
+            "Jill": "friend"
+        }
+    },
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "relations": {
+            "Jill": "partner"
+        }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "John": "cousin",
+            "Zalika": "landlord"
+        }
+    }
+}
+
+with open('group.json', 'w+') as fh:
+    json.dump(group, fh)
+
+with open('group.json','r') as fh:
+    new_group = json.load(fh)
+
+print(new_group == group)

fff50970faeaac2dfb9840457d8c88ee0a0ccc61
Ed Harris <zcqseoc@ucl.ac.uk>
2020-10-29 12:17:24 +0000

    Adding Data Viz of EQ

--- week04/quakes.py
+++ week04/quakes.py
@@ -5,6 +5,8 @@
 
 import json
 import requests
+import datetime
+import matplotlib.pyplot as plt
 
 # If you want, you can define some functions to help organise your code.
 # def helper_function(argument_1, argument_2):
@@ -18,13 +20,13 @@ if __name__ == "__main__":
 
     quakes = requests.get("http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
                       params={
-                          'starttime': "2000-01-01",
+                          'starttime': "1900-01-01",
                           "maxlatitude": "58.723",
                           "minlatitude": "50.008",
                           "maxlongitude": "1.67",
                           "minlongitude": "-9.756",
                           "minmagnitude": "1",
-                          "endtime": "2018-10-11",
+                          "endtime": "2020-10-11",
                           "orderby": "time-asc"}
                       )
     
@@ -41,6 +43,34 @@ if __name__ == "__main__":
     for quake in quakes_data:
         if quake['properties']['mag'] == max_magnitude:
             coords.append(quake['geometry']['coordinates'])
+    
+    quakes_by_year = {}
+    
+    for quake in quakes_data:
+        time = quake['properties']['time']
+        time = datetime.datetime.fromtimestamp(time/1000)
+        if time.year not in quakes_by_year:
+            quakes_by_year[time.year] = {"number": 0, "magnitude_average": 0}
+        quakes_by_year[time.year]["number"] +=1
+        quakes_by_year[time.year]["magnitude_average"] = (quakes_by_year[time.year]['magnitude_average'] \
+                                                        * (quakes_by_year[time.year]["number"] -1) \
+                                                        + (quake['properties']['mag'])) \
+                                                            / (quakes_by_year[time.year]["number"])
+    
+    x = [k for k in quakes_by_year]
+    y = [k["number"] for k in quakes_by_year.values()]
+    z = [k["magnitude_average"] for k in quakes_by_year.values()]
+    print(z)
+
+    fig, axs = plt.subplots(2, sharex=True)
+    axs[0].plot(x,z)
+    axs[1].plot(x,y)
+    axs[0].set_ylabel("Average Magnitude")
+    axs[1].set_ylabel("Frequency")
+    axs[1].set_xlabel("Year")
+
+    plt.savefig("Quakes_Data_Viz.png")
+
 
 
 

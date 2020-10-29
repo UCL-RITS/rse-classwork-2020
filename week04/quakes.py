4ee12bea0958c4aa46f0c3f929335ed1aecd4ec2
Evan Sutcliffe <30503110+evansutcliffe@users.noreply.github.com>
2020-10-29 12:13:13 +0000

    plot earthquake frequency

--- week04/quakes.py
+++ week04/quakes.py
@@ -2,6 +2,12 @@
 
 import requests
 import json
+import datetime
+import numpy as np 
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+
 
 # If you want, you can define some functions to help organise your code.
 # def helper_function(argument_1, argument_2):
@@ -26,18 +32,43 @@ if __name__ == "__main__":
 
     requests_json = json.loads(quakes.text)
 
+    #"time": 1579759019614,
+
+
+    years = set([ datetime.date.fromtimestamp(x["properties"]["time"] / 1e3).year for x in requests_json["features"]])
+
+    frequency_per_year = { year : len([earthquake for earthquake in requests_json["features"] if datetime.date.fromtimestamp(earthquake["properties"]["time"] / 1e3).year == year]) for year in years }
+    
+    data = list(frequency_per_year.items())
+    an_array = np.array(data)
+    sorted_array = an_array[np.argsort(an_array[:, 0])]
+    #dataset = zip(an_array)
+
+
+    plt.plot(sorted_array[:,0],sorted_array[:,1])
+    plt.xlabel('Years')
+    plt.ylabel('Frequency')
+    plt.title('Earthquake Frequency in the UK')
+    plt.show()
+    #print(frequency_per_year)
+
+
+
+
+    #print(requests_json['features'][0])
     #dict_keys = requests_json.keys()
 
-    max_magnitude = requests_json['features'][0]['properties']['mag']
-    max_data = requests_json['features'][0]
-    for data in  requests_json['features']:
-        magnitude = data['properties']['mag']
-        if (magnitude>max_magnitude):
-            #print(magnitude)
-            max_magnitude = magnitude
-            max_data = data
+    # max_magnitude = requests_json['features'][0]['properties']['mag']
+    # max_data = requests_json['features'][0]
+    # for data in  requests_json['features']:
 
-    coords = max_data['geometry']['coordinates'][0:2]
-    #long, lat  = coords
-    print(f"The maximum magnitude is {max_magnitude} "
-          f"and it occured at coordinates {coords}.")
+        # if (magnitude>max_magnitude):
+        #     #print(magnitude)
+        #     max_magnitude = magnitude
+        #     max_data = data
+
+    # coords = max_data['geometry']['coordinates'][0:2]
+    # #long, lat  = coords
+    # print(f"The maximum magnitude is {max_magnitude} "
+    #       f"and it occured at coordinates {coords}.")
+    

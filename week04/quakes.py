9a0c53fd208bc4d98dbbb298f6d3b279f306f5c0
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2020-10-29 12:32:55 +0000

    plts for freq and averge magnitude over yrs

--- week04/quakes.py
+++ week04/quakes.py
@@ -2,6 +2,10 @@
 
 import requests
 import json
+from datetime import date
+import copy
+from matplotlib import pyplot as plt
+import numpy as np
 
 quakes = requests.get("http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
                       params={
@@ -16,7 +20,25 @@ quakes = requests.get("http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
                       )
 
 quakes_json = json.loads(quakes.text)
+# with open('quakes.json', 'w') as file:
+#     json.dump(quakes_json, file)
 quakes_lst = quakes_json['features']
+
+quake_yrs = [ date.fromtimestamp(quake['properties']['time']/1000).year for quake in quakes_lst ]
+quake_mags = { yr : np.mean([ quake['properties']['mag'] for quake in quakes_lst if date.fromtimestamp(quake['properties']['time']/1000).year == yr]) for yr in quake_yrs }
+
+fig, axs = plt.subplots(1, 2)
+
+axs[0].hist(quake_yrs)
+axs[0].set_ylabel('frequency')
+axs[0].set_xlabel('yr')
+
+axs[1].plot(quake_mags.keys(), quake_mags.values())
+axs[1].set_ylabel('avg_mag')
+axs[1].set_xlabel('yr')
+
+plt.tight_layout()
+plt.show()
 
 max_mag = max([ quake['properties']['mag'] for quake in quakes_lst ])
 max_mag_idx = [ idx for idx, val in enumerate([ quake['properties']['mag'] for quake in quakes_lst ]) if val == max_mag ]

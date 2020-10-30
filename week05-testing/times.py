179009f9deaf365ce75d64d3dda3797f698aaac7
Sankalp Tipnis <35152001+sankalptipnis@users.noreply.github.com>
2020-10-30 20:37:13 +0000

    fix typo UCL-RITS/rse-classwork-2020#61

--- week05-testing/times.py
+++ week05-testing/times.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")

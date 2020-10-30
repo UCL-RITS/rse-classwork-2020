24b305650d5a6c48d2eb4df041b3f65c68e6ae4f
FMAUR92 <zcemfma@ucl.ac.uk>
2020-10-30 18:27:43 +0000

    Added test file, Answers UCL-RITS/rsd-classwork-2020#61

---
+++ week05-testing/test_times.py
@@ -0,0 +1,12 @@
+# Test file for the times.py file
+
+from times import time_range, compute_overlap_time # Importing the necessary functions from times.py
+
+# Testing that the output of compute_overlap_time is as expected 
+def test_given_input():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    
+    result = compute_overlap_time(large, short)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    assert result == expected

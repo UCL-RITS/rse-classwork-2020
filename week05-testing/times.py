fac2a9131573a72bb85346b94b896dc5f028b81b
Emre <rmapeta@ucl.ac.uk>
2020-10-31 21:53:45 +0300

    Times.py

--- week05-testing/times.py
+++ week05-testing/times.py
@@ -23,3 +23,6 @@ if __name__ == "__main__":
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     print(compute_overlap_time(large, short))
+
+#print(large)
+#print(short)

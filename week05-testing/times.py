10a5cf53a22a979da4aba244a7ae950993fed2bb
Vivian <vivian.barli.17@ucl.ac.uk>
2020-11-02 11:57:36 +0000

    Adding comments to the existing code

--- week05-testing/times.py
+++ week05-testing/times.py
@@ -1,15 +1,21 @@
-mport datetime
+import datetime
 
 
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+    # strptime converts string information to date. 
+    # the information after just tells us about the format of the date. Year, month, day, hour, minute, second
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+    # same thing as start_time
     d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
+    # total_seconds() figures out seconds between two times. the first part figures out how many blocks of time there are 
+    # 
     sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
                   start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
                  for i in range(number_of_intervals)]
     return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
-
+    # timedelta is a function that can be used to calculate about future or past. https://www.guru99.com/date-time-and-datetime-classes-in-python.html
+    # strftime is printing command 
 
 def compute_overlap_time(range1, range2):
     overlap_time = []
@@ -23,4 +29,4 @@ def compute_overlap_time(range1, range2):
 if __name__ == "__main__":
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    print(compute_overlap_time(large, short))
+    print("abc", compute_overlap_time(large, short))

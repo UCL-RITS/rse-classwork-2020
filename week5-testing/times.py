6b5cf98230b65f041f6111e645ba30468b848257
David Scobie <david.scobie.20@ucl.ac.uk>
2020-11-04 21:57:16 +0000

    Adding the test_times.py file to git so that times.py can be tested Answers UCL-RITS/rsd-classwork-2020#61

--- week5-testing/times.py
+++ week5-testing/times.py
@@ -15,6 +15,7 @@ def time_range(start_time, end_time, number_of_intervals=1, gap_between_interval
     sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
                   start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
                  for i in range(number_of_intervals)]
+    
     return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
 
 

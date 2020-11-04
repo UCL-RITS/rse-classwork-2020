2dbc359bd3333c427e1a5980efdc8bd10f6474f3
premal-varsani <ucappv0@ucl.ac.uk>
2020-11-04 17:48:50 +0000

    added more tests to the test_times file

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -7,3 +7,17 @@ def test_given_input():
     result = compute_overlap_time(large,short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+    large2 = time_range("2008-01-12 10:00:00", "2010-01-12 12:00:00")
+    short2 = time_range("2008-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = compute_overlap_time(large2,short2)
+    expected = [('2008-01-12 10:30:00', '2009-01-11 22:37:00'), ('2009-01-11 22:38:00', '2010-01-12 10:45:00')]
+    assert result == expected
+
+    large3 = time_range("2010/01/12 10:00:00", "2010-01-12 12:00:00")
+    short3 = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = compute_overlap_time(large3,short3)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    assert result == expected
+
+

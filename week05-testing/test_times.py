8428cc1d99e8734d8b63abf11b699d1ae237bfc5
Thomas Wemyss <twemyss@users.noreply.github.com>
2020-11-05 11:01:47 +0000

    Add edge case tests and fixes. Answers UCL-RITS/rse-classwork-2020#86

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -9,3 +9,27 @@ class TestTimeOverlap:
         result = times.compute_overlap_time(times.large, times.short)
         expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
         assert result == expected
+
+    def test_non_overlapping_times(self):
+        """Tests two time ranges that do not overlap"""
+        t1 = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+        t2 = times.time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00")
+        result = times.compute_overlap_time(t1, t2)
+        expected = []
+        assert result == expected
+
+    def test_several_intervals(self):
+        """Tests intervals where two intervals overlap"""
+        t1 = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 0)
+        t2 = times.time_range("2010-01-12 10:30:00", "2010-01-12 11:30:00", 2, 0)
+        result = times.compute_overlap_time(t1, t2)
+        expected = [('2010-01-12 10:30:00', '2010-01-12 11:00:00'),('2010-01-12 11:00:00', '2010-01-12 11:30:00')]
+        assert result == expected
+
+    def test_touching_times(self):
+        """Tests intervals where one finishes at the same time another starts"""
+        t1 = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+        t2 = times.time_range("2010-01-12 12:00:00", "2010-01-12 12:30:00")
+        result = times.compute_overlap_time(t1, t2)
+        expected = []
+        assert result == expected

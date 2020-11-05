567c02525bae03f1199dc0a1fab3c971021ddd81
AlexWilkinsonn <alexander.wilkinson.20@ucl.ac.uk>
2020-11-05 13:22:00 +0000

    Store parameters in a yaml file

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,10 +1,20 @@
 from times import time_range, compute_overlap_time
 import pytest
+import yaml
 
-@pytest.mark.parametrize("time_range_1, time_range_2, expected", 
-                        [(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60), [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')])
-                        , (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00"), [])
-                        , (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 12:00:00", "2010-01-12 13:00:00"), [])])
+test_params = {
+    'generic' : (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60), [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]),
+    'no overlap' : (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00"), []),
+    'exact  overlap' : (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 12:00:00", "2010-01-12 13:00:00"), [])
+}
+
+with open('fixture.yaml', 'w') as file:
+    yaml.dump(test_params, file)
+
+with open('fixture.yaml', 'r') as file:
+    fixture_dict = yaml.load(file, Loader=yaml.FullLoader)
+
+@pytest.mark.parametrize("time_range_1, time_range_2, expected", list(fixture_dict.values()), ids=fixture_dict.keys())
 def test_given_input(time_range_1, time_range_2, expected):
 
     result = compute_overlap_time(time_range_1, time_range_2)

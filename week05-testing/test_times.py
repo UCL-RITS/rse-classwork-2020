560b0619769349141a74b45396ee1a487975ea6b
Aixi <=>
2020-11-05 19:56:09 +0800

    test about no overlap and backfoward date

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -10,9 +10,9 @@ def test_given_input():
 
 def test_no_overlap():
     large = time_range("2010-01-12 11:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00")
     result = compute_overlap_time(large, short)
-    expected = ''
+    expected = []
     assert result == expected
 
 

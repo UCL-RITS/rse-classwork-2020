4f843d3c0603ad319295491fa0d6bccbc2af504c
Aixi <=>
2020-11-05 17:22:11 +0800

    Answers UCL-RITS/rsd-classwork-2020#61

--- week05-testing/times.py
+++ week05-testing/times.py
@@ -1,6 +1,5 @@
 import datetime
 
-
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
@@ -20,7 +19,7 @@ def compute_overlap_time(range1, range2):
             overlap_time.append((low, high))
     return overlap_time
 
-if __name__ == "__main__":
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    print(compute_overlap_time(large, short))
+# if __name__ == "__main__":
+#     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+#     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+#     print(compute_overlap_time(large, short))

39fd665ec7802c82ab2e1d6b194e261d3cb1e15b
premal-varsani <ucappv0@ucl.ac.uk>
2020-11-05 11:47:16 +0000

    negative tests

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -8,19 +8,19 @@ def test_given_input():
     expected = [('2010-01-12 12:30:00', '2010-01-12 12:00:00'), ('2010-01-12 12:38:00', '2010-01-12 12:00:00')]
     assert result == expected
 
- def test_no_overlap():
+ #def test_no_overlap():
     #Time ranges that do not overlap
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    result = compute_overlap_time(large,short)
-    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-    assert result == expected
+    #large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    #short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    #result = compute_overlap_time(large,short)
+    #expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    #assert result == expected
 
-  def test_sev_int():  
+def test_sev_int():  
     #Time ranges with several intervals
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00",2,0)
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 0)
-    result = compute_overlap_time(large3,short3)
+    result = compute_overlap_time(large,short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:30'), ('2010-01-12 10:37:30', '2010-01-12 10:45:00'), ('2010-01-12 11:00:00', '2010-01-12 10:37:30'), ('2010-01-12 11:00:00', '2010-01-12 10:45:00')]
     assert result == expected
 
@@ -31,4 +31,12 @@ def test_touching_times():
     result = compute_overlap_time(large,short)
     expected = [('2010-01-12 12:00:00', '2010-01-12 12:00:00'), ('2010-01-12 12:23:00', '2010-01-12 12:00:00')]
     assert result == expected
+
+def negative_test():
+    t = time_range("2010-01-12 12:00:00", "2010-01-12 10:00:00")
+    with pytest.raises(ValueError):
+        compute_overlap_time(t)
+    #result = compute_overlap_time(large,short)
+    #expected = [('2010-01-12 12:30:00', '2010-01-12 12:00:00'), ('2010-01-12 12:38:00', '2010-01-12 12:00:00')]
+    #assert result == expected
 

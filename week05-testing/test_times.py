b1a42f568118e69153a147e3c1867464e27c72af
Aure <zcemaen@ucl.ac.uk>
2020-11-05 10:59:07 +0000

    adding first test for times.py

---
+++ week05-testing/test_times.py
@@ -0,0 +1,14 @@
+from times import compute_overlap_time
+from times import time_range
+
+
+def test_given_input():
+    if __name__ == "__main__":
+        large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+
+        short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+
+        result = compute_overlap_time(large, short)
+        expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+                    ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+        assert result == expected
--- week05-testing/times.py
+++ week05-testing/times.py
@@ -24,5 +24,7 @@ def compute_overlap_time(range1, range2):
 
 if __name__ == "__main__":
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    print(large)
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    print(short)
     print(compute_overlap_time(large, short))

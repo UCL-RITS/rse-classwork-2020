34fafd3f67562b875a68b39d57cd8b9334a1debd
goldsmdn <daniel.goldsmith.20@ucl.ac.uk>
2020-11-05 11:50:00 +0000

    Classwork

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,4 +1,5 @@
 import times
+from pytest import raises
 
 def test_given_input():
 
@@ -8,3 +9,29 @@ def test_given_input():
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
 
     assert result == expected
+
+def test_non_overlap():
+    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = times.time_range("2010-01-12 14:30:00", "2010-01-12 14:45:00", 2, 60)
+    result = (times.compute_overlap_time(large, short))
+    print (result)
+    expected = []
+
+    assert result == expected
+
+def test_several_intervals():
+    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00", 2, 60)
+    short = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = (times.compute_overlap_time(large, short))
+    print (result)
+    expected = [('2010-01-12 10:30:30', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+
+    assert result == expected
+
+def test_time_back():
+
+    start_time = "2010-01-12 12:00:00"
+    end_time = "2010-01-12 10:00:00"
+
+    with raises(ValueError):
+        times.time_range(start_time, end_time)

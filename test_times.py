38f23deb30e033208dce8c09a4aa70a9a8062e85
Jed <jedhmr@gmail.com>
2020-11-05 10:37:54 +0000

    add initial timing files

---
+++ test_times.py
@@ -0,0 +1,7 @@
+from times import compute_overlap_time, time_range
+
+def test_generic_case():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
+    assert compute_overlap_time(large, short) == expected

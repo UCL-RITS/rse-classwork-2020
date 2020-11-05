e2bdc7787275ee4b6ac1468241fe0f258ef04b9f
Ahmad Abdulrazzak <ucapbdb@ucl.ac.uk>
2020-11-05 11:09:18 +0000

    Write tests for edge cases

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -7,5 +7,25 @@ def test_given_input():
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     result = compute_overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-    assert result ==  expected
+    assert result == expected
 
+def test_not_overlapping():
+    range1 = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    range2 = time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00")
+    result = compute_overlap_time(range1, range2)
+    expected = 0
+    assert result == expected
+
+def test_multiple_intervals():
+    range1 = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00", 2, 60)
+    range2 = time_range("2010-01-12 10:50:00", "2010-01-12 11:01:00", 2, 120)
+    expected = [('2010-01-12 10:50:00', '2010-01-12 10:54:30'), ('2010-01-12 10:56:30', '2010-01-12 11:00:00')]
+    result = compute_overlap_time(range1, range2)
+    assert result == expected
+
+def test_end_start():
+    range1 = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00")
+    range2 = time_range("2010-01-12 11:00:00", "2010-01-12 12:00:00")
+    expected = [('2010-01-12 11:00:00', '2010-01-12 11:00:00')]
+    result = compute_overlap_time(range1, range2)
+    assert expected == result
--- week05-testing/times.py
+++ week05-testing/times.py
@@ -18,4 +18,5 @@ def compute_overlap_time(range1, range2):
             low = max(start1, start2)
             high = min(end1, end2)
             overlap_time.append((low, high))
+
     return overlap_time

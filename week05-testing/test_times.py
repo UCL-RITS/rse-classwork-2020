16098c0793d510f59706abc8ada7933651c7841d
Sankalp Tipnis <35152001+sankalptipnis@users.noreply.github.com>
2020-11-05 13:59:58 +0000

    Format tests + update .gitignore + add travis CI

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,68 +1,130 @@
 import times
+import pytest
+import datetime
 
-def test_given_input():
+@pytest.mark.parametrize("test_input1, test_input2, expected", [
+    # no ovelap
+    (('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+     ('2010-01-12 10:38:00', '2010-01-12 12:20:00'), 
+     tuple()),
 
-    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    # inside edge overlap
+    (('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+     ('2010-01-12 10:30:00', '2010-01-12 10:35:00'),
+     ('2010-01-12 10:30:00', '2010-01-12 10:35:00')),
+
+    # equal intervals
+    (('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+     ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+     ('2010-01-12 10:30:00', '2010-01-12 10:37:00')),
+
+    # outside edge overlap
+    (('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+     ('2010-01-12 10:37:00', '2010-01-12 10:39:00'),
+     tuple()),
+
+    # partial overlap
+    (('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+     ('2010-01-12 10:34:00', '2010-01-12 10:42:00'),
+     ('2010-01-12 10:34:00', '2010-01-12 10:37:00')),
+
+    # subset overlap
+    (('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+     ('2010-01-12 10:32:00', '2010-01-12 10:35:00'),
+     ('2010-01-12 10:32:00', '2010-01-12 10:35:00')),
+
+])
+def test_overlap_helper(test_input1, test_input2, expected):
+    assert times.compute_overlap_helper(test_input1, test_input2) == expected
+
+
+@pytest.mark.parametrize("test_input1, test_input2, expected", [
+    # inverted interval
+    (('2010-01-12 10:35:00', '2010-01-12 10:32:00'),
+     ('2010-01-12 10:38:00', '2010-01-12 12:20:00'),
+     pytest.raises(ValueError)),
+
+    # non-tuple input
+    (['2010-01-12 10:30:00', '2010-01-12 10:37:00'],
+     ['2010-01-12 10:34:00', '2010-01-12 10:42:00'],
+     pytest.raises(TypeError)),
+
+    # tuple interval of length != 2
+    (('2010-01-12 10:35:00', '2010-01-12 10:32:00', '2010-01-12 10:39:00'),
+     ('2010-01-12 10:38:00', '2010-01-12 12:20:00'),
+     pytest.raises(TypeError)),
+
+    # star and end of tuple interval of different datatypes
+    (('2010-01-12 10:35:00', datetime.datetime.strptime('2010-01-12 10:39:00', "%Y-%m-%d %H:%M:%S")),
+     ('2010-01-12 10:34:00', '2010-01-12 12:20:00'),
+     pytest.raises(TypeError)),
+
+])
+def test_overlap_helper_errors(test_input1, test_input2, expected):
+    with expected:
+        times.compute_overlap_helper(test_input1, test_input2)
+
+# def test_no_overlap():
+#     assert(
+#         times.compute_overlap_helper(
+#             ('2010-01-12 10:30:00', '2010-01-12 10:37:00'), 
+#             ('2010-01-12 10:38:00', '2010-01-12 12:20:00')
+#         ) 
+#         == tuple()
+#     )
+
+# def test_inside_edge_overlap():
+#     assert(
+#         times.compute_overlap_helper(
+#             ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+#             ('2010-01-12 10:30:00', '2010-01-12 10:35:00')
+#         )
+#         == ('2010-01-12 10:30:00', '2010-01-12 10:35:00')
+#     )
+
+
+# def test_same_overlap():
+#     assert(
+#         times.compute_overlap_helper(
+#             ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+#             ('2010-01-12 10:30:00', '2010-01-12 10:37:00')
+#         )
+#         == ('2010-01-12 10:30:00', '2010-01-12 10:37:00')
+#     )
+
+# def test_outside_edge_overlap():
+#     assert(
+#         times.compute_overlap_helper(
+#             ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+#             ('2010-01-12 10:37:00', '2010-01-12 10:39:00')
+#         )
+#         == tuple()
+#     )
+
+# def test_partial_overlap():
+#     assert(
+#         times.compute_overlap_helper(
+#             ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+#             ('2010-01-12 10:34:00', '2010-01-12 10:42:00')
+#         )
+#         == ('2010-01-12 10:34:00', '2010-01-12 10:37:00')
+#     )
+
+# def test_subset_overlap():
+#     assert(
+#         times.compute_overlap_helper(
+#             ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+#             ('2010-01-12 10:32:00', '2010-01-12 10:36:30')
+#         )
+#         == ('2010-01-12 10:32:00', '2010-01-12 10:36:30')
+#     )
+
+
+# def test_inverted_interval():
+#     with pytest.raises(ValueError) as exception:
+#         times.compute_overlap_helper(
+#             ('2010-01-12 10:35:00', '2010-01-12 10:30:00'),
+#             ('2010-01-12 10:32:00', '2010-01-12 10:36:30')
+#         )
+        
     
-    result = times.compute_overlap_time(large, short)
-    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
-                ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-  
-    assert result == expected
-
-def test_no_overlap_helper():
-    assert(
-        times.compute_overlap_helper(
-            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'), 
-            ('2010-01-12 10:38:00', '2010-01-12 12:20:00')
-        ) 
-        == tuple()
-    )
-
-def test_inside_edge_overlap_helper():
-    assert(
-        times.compute_overlap_helper(
-            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
-            ('2010-01-12 10:30:00', '2010-01-12 10:35:00')
-        )
-        == ('2010-01-12 10:30:00', '2010-01-12 10:35:00')
-    )
-
-
-def test_same_overlap_helper():
-    assert(
-        times.compute_overlap_helper(
-            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
-            ('2010-01-12 10:30:00', '2010-01-12 10:37:00')
-        )
-        == ('2010-01-12 10:30:00', '2010-01-12 10:37:00')
-    )
-
-def test_outside_edge_overlap_helper():
-    assert(
-        times.compute_overlap_helper(
-            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
-            ('2010-01-12 10:37:00', '2010-01-12 10:39:00')
-        )
-        == tuple()
-    )
-
-def test_partial_overlap_helper():
-    assert(
-        times.compute_overlap_helper(
-            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
-            ('2010-01-12 10:34:00', '2010-01-12 10:42:00')
-        )
-        == ('2010-01-12 10:34:00', '2010-01-12 10:37:00')
-    )
-
-def test_subset_overlap_helper():
-    assert(
-        times.compute_overlap_helper(
-            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
-            ('2010-01-12 10:32:00', '2010-01-12 10:36:30')
-        )
-        == ('2010-01-12 10:32:00', '2010-01-12 10:36:30')
-    )
-
--- week05-testing/times.py
+++ week05-testing/times.py
@@ -1,6 +1,5 @@
 import datetime
 
-
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
@@ -10,9 +9,23 @@ def time_range(start_time, end_time, number_of_intervals=1, gap_between_interval
                  for i in range(number_of_intervals)]
     return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
 
-def compute_overlap_helper(tuple1, tuple2):
-    start1, end1 = tuple1
-    start2, end2 = tuple2
+def compute_overlap_helper(interval1, interval2):
+    
+    if(not(type(interval1) == type(interval2) == type(tuple()))):
+        raise(TypeError("Intervals must be tuples."))
+    
+    if(not(len(interval1) == len(interval2)==2)):
+        raise(TypeError("Intervals must be tuples with two elements of the form (start, end)."))
+    
+    start1, end1 = interval1
+    start2, end2 = interval2
+
+    if(not(type(start1) == type(end1) == type(start2) == type(end2))):
+        raise TypeError("The start and end of both intervals must be of the same type.")
+    
+    if(start1 > end1 or start2 > end2):
+        raise ValueError("The start of an interval cannot be after the end of an interval.")
+
     if(start1 >= end2 or start2 >= end1):
         return tuple()
     else:
@@ -27,3 +40,6 @@ def compute_overlap_time(range1, range2):
             if(overlap := compute_overlap_helper(tuple1, tuple2)):
                 overlap_time.append((overlap))
     return(overlap_time)
+
+
+

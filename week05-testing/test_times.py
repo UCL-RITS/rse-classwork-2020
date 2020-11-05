5935677a345c81878ce1477f45ec2de2792f2fa3
Umit <60366307+umitozmen@users.noreply.github.com>
2020-11-05 11:57:22 +0000

    adding more test UCL-RITS/rse-classwork-2020#86

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -10,6 +10,61 @@ def test_given_input():
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
 
+def test_no_overlap_helper():
+    assert(
+        times.compute_overlap_helper(
+            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'), 
+            ('2010-01-12 10:38:00', '2010-01-12 12:20:00')
+        ) 
+        == tuple()
+    )
+
+def test_inside_edge_overlap_helper():
+    assert(
+        times.compute_overlap_helper(
+            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+            ('2010-01-12 10:30:00', '2010-01-12 10:35:00')
+        )
+        == ('2010-01-12 10:30:00', '2010-01-12 10:35:00')
+    )
+
+
+def test_same_overlap_helper():
+    assert(
+        times.compute_overlap_helper(
+            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+            ('2010-01-12 10:30:00', '2010-01-12 10:37:00')
+        )
+        == ('2010-01-12 10:30:00', '2010-01-12 10:37:00')
+    )
+
+def test_outside_edge_overlap_helper():
+    assert(
+        times.compute_overlap_helper(
+            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+            ('2010-01-12 10:37:00', '2010-01-12 10:39:00')
+        )
+        == tuple()
+    )
+
+def test_partial_overlap_helper():
+    assert(
+        times.compute_overlap_helper(
+            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+            ('2010-01-12 10:34:00', '2010-01-12 10:42:00')
+        )
+        == ('2010-01-12 10:34:00', '2010-01-12 10:37:00')
+    )
+
+def test_subset_overlap_helper():
+    assert(
+        times.compute_overlap_helper(
+            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
+            ('2010-01-12 10:32:00', '2010-01-12 10:36:30')
+        )
+        == ('2010-01-12 10:32:00', '2010-01-12 10:36:30')
+    )
+    
 def test_negative():
     with raises(ValueError):
         time_range("2010-01-12 12:00:00", "2010-01-12 10:00:00")

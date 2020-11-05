72f9c3334443aa62af4d3fedf6ed88f8b3b885b7
AlexWilkinsonn <alexander.wilkinson.20@ucl.ac.uk>
2020-11-05 13:24:01 +0000

    Forgot to delete some guff

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -2,15 +2,6 @@ from times import time_range, compute_overlap_time
 import pytest
 import yaml
 
-test_params = {
-    'generic' : (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60), [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]),
-    'no overlap' : (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00"), []),
-    'exact  overlap' : (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 12:00:00", "2010-01-12 13:00:00"), [])
-}
-
-with open('fixture.yaml', 'w') as file:
-    yaml.dump(test_params, file)
-
 with open('fixture.yaml', 'r') as file:
     fixture_dict = yaml.load(file, Loader=yaml.FullLoader)
 

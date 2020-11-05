50d35b8360af5ff40e216dff0772832fbf8e28a7
Ahmad Abdulrazzak <ucapbdb@ucl.ac.uk>
2020-11-05 13:59:15 +0000

    added additional test

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -3,6 +3,7 @@ import datetime
 import pytest
 from pytest import raises
 
+
 @pytest.mark.parametrize("time_range_1, time_range_2, expected", [
 
     # test given input
@@ -15,7 +16,12 @@ from pytest import raises
     time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00"),
     []), 
 
-    # test for time ranges with multiple intervals
+    # test for time ranges with one range having multiple intervals
+    (time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00"),
+    time_range("2010-01-12 10:50:00", "2010-01-12 11:01:00", 2, 120),
+    [('2010-01-12 10:50:00', '2010-01-12 10:54:30'), ('2010-01-12 10:56:30', '2010-01-12 11:00:00')]),
+
+    # test for time ranges with both having multiple intervals
     (time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00", 2, 60),
     time_range("2010-01-12 10:50:00", "2010-01-12 11:01:00", 2, 120),
     [('2010-01-12 10:50:00', '2010-01-12 10:54:30'), ('2010-01-12 10:56:30', '2010-01-12 11:00:00')]),

86c2ead104fd1c1eb3985ac71e8dcedda3786ebc
Umit <60366307+umitozmen@users.noreply.github.com>
2020-11-05 11:53:16 +0000

    Negative Test UCL-RITS/rse-classwork-2020#88

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,4 +1,5 @@
 from times import time_range ,compute_overlap_time
+from pytest import raises
 
 def test_given_input():
     
@@ -8,3 +9,7 @@ def test_given_input():
     result = compute_overlap_time(large, short) 
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+def test_negative():
+    with raises(ValueError):
+        time_range("2010-01-12 12:00:00", "2010-01-12 10:00:00")

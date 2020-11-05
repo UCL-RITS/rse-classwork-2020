c36ef258046ec77298171d7133243c45dd601d7e
JingJiao <ucapjji@ucl.ac.uk>
2020-11-05 15:59:24 +0800

    deleted a line of code

--- test_times.py
+++ test_times.py
@@ -1,7 +1,6 @@
 from times import time_range, compute_overlap_time
- from datetime import datetime
 
- def test_given_input():
+def test_given_input():
 
      large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
      short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)

3a135dfc870273c3f2141c3fce2cfd57552bb3e7
JingJiao <ucapjji@ucl.ac.uk>
2020-11-05 15:50:46 +0800

    the second time to add test_times.py

--- test_times.py
+++ test_times.py
@@ -6,6 +6,6 @@ from times import time_range, compute_overlap_time
      large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
      short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
      result = compute_overlap_time(large,short)
-     expect = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
 
      assert result == expected

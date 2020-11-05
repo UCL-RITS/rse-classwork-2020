7f88064700868e2ff0b4212983c23596b47807dc
cristinafni <wmhkcfi@ucl.ac.uk>
2020-11-05 14:12:48 +0000

    testing travis CI with yml file

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,16 +1,13 @@
 from times import compute_overlap_time, time_range
 import pytest
 
-
 @pytest.mark.parametrize('time_range1, time_range2, expected', [
         (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), 
         time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60),
         [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]),
-
         (time_range("2009-01-12 10:00:00", "2009-01-12 12:00:00"), 
         time_range("2001-01-12 10:30:00", "2001-01-12 10:45:00", 2, 60),
         [])
-
         ])
 
 def test_eval(time_range1, time_range2, expected):
@@ -21,5 +18,3 @@ def test_eval(time_range1, time_range2, expected):
 def test_backwards():
     with pytest.raises(ValueError):
         time_range("2010-01-12 12:00:00", "2010-01-12 10:00:00")
-
-

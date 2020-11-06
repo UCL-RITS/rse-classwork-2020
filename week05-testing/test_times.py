8f7b257c2ca2c3b7bde8fd638d5a93a2108d977c
Umit <60366307+umitozmen@users.noreply.github.com>
2020-11-06 11:02:12 +0000

    parametrize answer UCL-RITS/rse-classwork-2020#89

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,5 +1,20 @@
-from times import time_range ,compute_overlap_time
-from pytest import raises
+from times import time_range ,compute_overlap_time, compute_overlap_helper
+import pytest
+
+testdata = [[('2010-01-12 10:30:00', '2010-01-12 10:37:00'),('2010-01-12 10:38:00', '2010-01-12 12:20:00'),tuple()] #test_no_overlap_helper
+     ,(('2010-01-12 10:30:00', '2010-01-12 10:37:00'),('2010-01-12 10:30:00', '2010-01-12 10:35:00'),('2010-01-12 10:30:00', '2010-01-12 10:35:00')) #test_inside_edge_overlap_helper
+     ,(('2010-01-12 10:30:00', '2010-01-12 10:37:00'),('2010-01-12 10:30:00', '2010-01-12 10:37:00'),('2010-01-12 10:30:00', '2010-01-12 10:37:00')) #test_same_overlap_helper
+     ,(('2010-01-12 10:30:00', '2010-01-12 10:37:00'),('2010-01-12 10:37:00', '2010-01-12 10:39:00'),tuple()) #test_outside_edge_overlap_helper
+     ,(('2010-01-12 10:30:00', '2010-01-12 10:37:00'),('2010-01-12 10:34:00', '2010-01-12 10:42:00'),('2010-01-12 10:34:00', '2010-01-12 10:37:00')) #test_partial_overlap_helper
+     ,(('2010-01-12 10:30:00', '2010-01-12 10:37:00'),('2010-01-12 10:32:00', '2010-01-12 10:36:30'),('2010-01-12 10:32:00', '2010-01-12 10:36:30'))#test_subset_overlap_helper
+]
+
+@pytest.mark.parametrize(
+    "time_range1,time_range2,expected",testdata
+)
+
+def test_eval(time_range1,time_range2,expected):
+    assert compute_overlap_helper(time_range1,time_range2) == expected
 
 def test_given_input():
     
@@ -9,62 +24,16 @@ def test_given_input():
     result = compute_overlap_time(large, short) 
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
-
-def test_no_overlap_helper():
-    assert(
-        times.compute_overlap_helper(
-            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'), 
-            ('2010-01-12 10:38:00', '2010-01-12 12:20:00')
-        ) 
-        == tuple()
-    )
-
-def test_inside_edge_overlap_helper():
-    assert(
-        times.compute_overlap_helper(
-            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
-            ('2010-01-12 10:30:00', '2010-01-12 10:35:00')
-        )
-        == ('2010-01-12 10:30:00', '2010-01-12 10:35:00')
-    )
-
-
-def test_same_overlap_helper():
-    assert(
-        times.compute_overlap_helper(
-            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
-            ('2010-01-12 10:30:00', '2010-01-12 10:37:00')
-        )
-        == ('2010-01-12 10:30:00', '2010-01-12 10:37:00')
-    )
-
-def test_outside_edge_overlap_helper():
-    assert(
-        times.compute_overlap_helper(
-            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
-            ('2010-01-12 10:37:00', '2010-01-12 10:39:00')
-        )
-        == tuple()
-    )
-
-def test_partial_overlap_helper():
-    assert(
-        times.compute_overlap_helper(
-            ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
-            ('2010-01-12 10:34:00', '2010-01-12 10:42:00')
-        )
-        == ('2010-01-12 10:34:00', '2010-01-12 10:37:00')
-    )
-
+"""
 def test_subset_overlap_helper():
     assert(
-        times.compute_overlap_helper(
+        compute_overlap_helper(
             ('2010-01-12 10:30:00', '2010-01-12 10:37:00'),
             ('2010-01-12 10:32:00', '2010-01-12 10:36:30')
         )
         == ('2010-01-12 10:32:00', '2010-01-12 10:36:30')
     )
-    
+"""
 def test_negative():
-    with raises(ValueError):
+    with pytest.raises(ValueError):
         time_range("2010-01-12 12:00:00", "2010-01-12 10:00:00")

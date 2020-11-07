c0d7cd0581587f3f775d3203d9e4348e81f63915
goldsmdn <daniel.goldsmith.20@ucl.ac.uk>
2020-11-07 17:05:50 +0000

    YAML change

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,60 +1,45 @@
 import times
+import yaml
 import pytest
 from pytest import raises
 
-@pytest.mark.parametrize("first_range, second_range, expected",
- [
-    #base case
-    (
-        times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
-        times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60),
-        [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-    ),
-    #test non overlap
-   (
-        times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
-        times.time_range("2010-01-12 14:30:00", "2010-01-12 14:45:00", 2, 60),
-        []
-    ),     
-    #test several intervals
-   (
-        times.time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00", 2, 60),
-        times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60),
-        [('2010-01-12 10:30:30', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-   ) 
- ]
-)
+with open("fixture.yml", 'r') as yamlfile:
+    fixture = yaml.safe_load(yamlfile)
+    print(fixture)
 
-def test_eval(first_range, second_range, expected):
-    result = times.compute_overlap_time(first_range,second_range)
-    assert result == expected
+@pytest.mark.parametrize("test_name", fixture)
+# fixture is a list of dictionaries [{'generic':...}, {'no_overlap':...}, ...]
+ 
+# [
+#    #base case
+#    (
+#        times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
+#        times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60),
+#        [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+#    ),
+#    #test non overlap
+#   (
+#        times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
+#        times.time_range("2010-01-12 14:30:00", "2010-01-12 14:45:00", 2, 60),
+#        []
+#    ),     
+#    #test several intervals
+#   (
+#        times.time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00", 2, 60),
+#        times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60),
+#        [('2010-01-12 10:30:30', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+#   ) 
+# ]
+#)
 
-#def test_given_input():
+def test_eval(test_name):
+ # test_name will be a dictionary, e.g. for the first case: {'generic': {'time_range_1':..., 'time_range2':..., 'expected':...}
+    properties = list(test_name.values())[0]
+    first_range = times.time_range(*properties['time_range_1'])
+    second_range = times.time_range(*properties['time_range_2'])
+    expected = [(start, stop) for start, stop in properties['expected']]
+    assert times.compute_overlap_time(first_range, second_range) == expected
 
-#   large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-#    short = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-#    result = (times.compute_overlap_time(large, short))
-#    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-
-#    assert result == expected
-
-#def test_non_overlap():
-#    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-#    short = times.time_range("2010-01-12 14:30:00", "2010-01-12 14:45:00", 2, 60)
-#    result = (times.compute_overlap_time(large, short))
-#    print (result)
-#    expected = []
-
-#    assert result == expected
-
-#def test_several_intervals():
-#    large = times.time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00", 2, 60)
-#    short = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-#    result = (times.compute_overlap_time(large, short))
-#    print (result)
-#    expected = [('2010-01-12 10:30:30', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-#
-#    assert result == expected
 
 def test_time_back():
 

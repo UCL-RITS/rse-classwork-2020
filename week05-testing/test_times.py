251de14e0f08b36cae78edf72de1be6439ab463e
Emre <rmapeta@ucl.ac.uk>
2020-11-08 14:46:22 +0300

    Added negative test

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,5 +1,6 @@
 from times import compute_overlap_time
 from times import time_range
+from pytest import raises
 # import times as ts
 import datetime
 
@@ -28,7 +29,7 @@ def two_several_interval(): # two time ranges that both contain several interval
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')] #expected output
     assert result == expected
 
-def two_same_start_end(): # two time ranges that both contain several intervals each
+def two_same_start_end(): # start time = end time
     range1 = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
 
     range2 = time_range("2010-01-12 12:00:00", "2010-01-12 13:00:00")
@@ -36,6 +37,14 @@ def two_same_start_end(): # two time ranges that both contain several intervals 
     result = compute_overlap_time(range1,range2)
     expected = [] #expected output
     assert result == expected
+
+def backwards_negative_test(): # Negative tests - Test that something that is expected to fail actually does fail
+    range1 = time_range("2010-01-12 12:00:00", "2010-01-12 10:00:00")
+    range2 = time_range("2010-01-12 12:00:00", "2010-01-12 13:00:00")
+
+    with raises(ValueError):
+        compute_overlap_time(range1,range2)
+        
    
 # Testing area
 
@@ -43,3 +52,4 @@ def two_same_start_end(): # two time ranges that both contain several intervals 
 #two_no_overlap()
 #two_several_interval()
 #two_same_start_end()
+backwards_negative_test()
--- week05-testing/times.py
+++ week05-testing/times.py
@@ -1,8 +1,13 @@
 import datetime
+from pytest import raises
 
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+
+    if start_time_s > end_time_s:
+        raise ValueError('Start time is larger than end time!')  
+
     d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
     sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
                   start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
@@ -16,6 +21,7 @@ def compute_overlap_time(range1, range2):
         for start2, end2 in range2:
             low = max(start1, start2)
             high = min(end1, end2)
+
             if datetime.datetime.strptime(low, "%Y-%m-%d %H:%M:%S") < datetime.datetime.strptime(high, "%Y-%m-%d %H:%M:%S"): #fixed bug
                 overlap_time.append((low, high))
 

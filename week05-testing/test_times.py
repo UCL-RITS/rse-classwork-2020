86c3261f53d210f7c75cab496b89706694c0f7c4
Kin Ian Lo <keonlo123@gmail.com>
2020-11-09 16:17:21 +0000

    Add more tests

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,10 +1,28 @@
 from times import compute_overlap_time, time_range
 from datetime import datetime
+import pytest
 
-def test_given_input():
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    result = compute_overlap_time(large, short)
-    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-    
-    assert result == expected
+from times import compute_overlap_time, time_range
+
+@pytest.mark.parametrize("range1, range2, expected",
+[(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
+  time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60),
+  [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]),
+  (time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00"),
+  time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00", 2, 60),
+  []),
+  (time_range("2010-01-12 10:00:00", "2010-01-12 13:00:00", 3, 900),
+  time_range("2010-01-12 10:40:00", "2010-01-12 11:20:00", 2, 120),
+  [("2010-01-12 10:40:00","2010-01-12 10:50:00"), ("2010-01-12 11:05:00", "2010-01-12 11:20:00")]),
+  (time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00"),
+  time_range("2010-01-12 11:00:00", "2010-01-12 12:45:00"),
+  [])
+])
+def test_param(range1, range2, expected):
+    assert compute_overlap_time(range1, range2) == expected
+
+
+def test_backward():
+    with pytest.raises(ValueError) as e:
+        time_range("2010-01-12 13:00:00", "2010-01-12 12:00:00")
+        assert e.match('End time should be later than Start time.')

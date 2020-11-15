69332144397b713eaff80d9352f6d6e2e8c5ee78
matlee99 <72209995+matlee99@users.noreply.github.com>
2020-11-15 12:21:27 +0000

    Sample solution "write more tests"
    create a test each for:
    two time ranges that do not overlap
    two time ranges that both contain several intervals each
    two time ranges that end exactly at the same time when the other starts
    run pytest and see whether all tests are picked up by pytest and whether they pass.
    fix any bugs in times.py the test may have helped you find.
    Add the new and modified files to the repository, commit them (with a meaningful commit message that also includes
    Answers UCL-RITS/rsd-classwork-2020#86) and push it to your fork.

--- test_times.py
+++ test_times.py
@@ -5,3 +5,21 @@ def test_generic_case():
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
     assert compute_overlap_time(large, short) == expected
+
+def test_no_overlap():
+    before = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00")
+    after = time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00", 2, 60)
+    expected = []
+    assert compute_overlap_time(before, after) == expected
+
+def test_multiple_overlaps():
+    three_hours_with_two_fifteen_minute_breaks = time_range("2010-01-12 10:00:00", "2010-01-12 13:00:00", 3, 900)
+    two_ranges_splitting_the_first_break = time_range("2010-01-12 10:40:00", "2010-01-12 11:20:00", 2, 120)
+    expected = [("2010-01-12 10:40:00","2010-01-12 10:50:00"), ("2010-01-12 11:05:00", "2010-01-12 11:20:00")]
+    assert compute_overlap_time(three_hours_with_two_fifteen_minute_breaks, two_ranges_splitting_the_first_break) == expected
+
+def test_touching_edges():
+    before = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00")
+    after = time_range("2010-01-12 11:00:00", "2010-01-12 12:45:00")
+    expected = []
+    assert compute_overlap_time(before, after) == expected

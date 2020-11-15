b3ec98f6838c92433edf1a6a849ee203b15940ca
joana <joana.teixeira.19@ucl.ac.uk>
2020-11-15 17:18:54 +0000

    Corrected overlap to correctly account for touching but not overlaping intervals

--- week005-testing/times.py
+++ week005-testing/times.py
@@ -15,12 +15,12 @@ def compute_overlap_time(range1, range2):
     overlap_time = []
     for start1, end1 in range1:
         for start2, end2 in range2:
-            if end1 < start2:
+            if (end1 <= start2) or (end2 <= start1):
                 pass
-            elif (end1 == start2):
-                overlap_time.append(end1)
-            elif (end2 == start1): 
-                overlap_time.append(end2)
+            #elif (end1 == start2):
+            #    overlap_time.append(end1)
+            #elif (end2 == start1): 
+            #    overlap_time.append(end2)
             else:
                 low = max(start1, start2)
                 high = min(end1, end2)

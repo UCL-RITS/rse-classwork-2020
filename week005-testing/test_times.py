84a12e96d380c510c03f04398bf8edbd4950243d
joana <joana.teixeira.19@ucl.ac.uk>
2020-11-15 17:15:45 +0000

    Deleted old tests from the test_times file, which dont use parametrize.

--- week005-testing/test_times.py
+++ week005-testing/test_times.py
@@ -17,36 +17,3 @@ def test_general_overlap(test_name):
     print(properties)
     assert compute_overlap_time(first_range, second_range) == expected_overlap
     
-
-#def test_given_input():
-
- #   large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
- #   short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
- #   
- #   result = compute_overlap_time(large, short)
- #   expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
- #   assert result == expected
-
-#def test_no_overlap():
-
-#    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-#    short = time_range("2010-01-13 10:30:00", "2010-01-13 10:45:00")
-#    
-#    result = compute_overlap_time(large, short)
-#    expected = []
-#    assert result == expected
-
-#def test_later_start_time():
-#    with raises(ValueError) as exception:
-#        time_range("2010-01-12 10:00:00", "2010-01-10 12:00:00")
-
-
-#large1=time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-#small1 = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-#expected1 = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-
-#large2 = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-#small2 = time_range("2010-01-13 10:30:00", "2010-01-13 10:45:00")
-#expected2 = []
-
-

1badebcc4a15c40a218aef1f0a022a37f5d70123
wjl13910 <836823863@qq.com>
2020-11-18 01:29:15 +0800

    exercise

--- week06/average-squares-example/average_squares/squares.py
+++ week06/average-squares-example/average_squares/squares.py
@@ -3,10 +3,10 @@
 
 def average_of_squares(list_of_numbers, list_of_weights=None):
     """ Return the weighted average of a list of values.
-    
+
     By default, all values are equally weighted, but this can be changed
     by the list_of_weights argument.
-    
+
     Example:
     --------
     >>> average_of_squares([1, 2, 4])
@@ -29,16 +29,16 @@ def average_of_squares(list_of_numbers, list_of_weights=None):
         for number, weight
         in zip(list_of_numbers, effective_weights)
     ]
-    return sum(squares)
+    return sum(squares)/len(list_of_numbers)
 
 
 def convert_numbers(list_of_strings):
     """Convert a list of strings into numbers, ignoring whitespace.
-    
+
     Example:
     --------
     >>> convert_numbers(["4", " 8 ", "15 16", " 23    42 "])
-    [4, 8, 15, 16]
+    [4.0, 8.0, 15.0, 16.0, 23.0, 42.0]
 
     """
     all_numbers = []
@@ -51,12 +51,12 @@ def convert_numbers(list_of_strings):
 
 
 if __name__ == "__main__":
-    numbers_strings = ["1","2","4"]
-    weight_strings = ["1","1","1"]        
-    
+    numbers_strings = ["1", "2", "4"]
+    weight_strings = ["1", "1", "1"]
+
     numbers = convert_numbers(numbers_strings)
     weights = convert_numbers(weight_strings)
-    
+
     result = average_of_squares(numbers, weights)
-    
+
     print(result)

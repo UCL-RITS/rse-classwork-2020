51f452363eafab727a7883e61ef8fb156d1a452e
joana <joana.teixeira.19@ucl.ac.uk>
2020-11-19 08:36:39 +0000

    corrected docstrings and some faulty code on squares.py

--- week06/average-squares-example/average_squares/squares.py
+++ week06/average-squares-example/average_squares/squares.py
@@ -12,7 +12,7 @@ def average_of_squares(list_of_numbers, list_of_weights=None):
     >>> average_of_squares([1, 2, 4])
     7.0
     >>> average_of_squares([2, 4], [1, 0.5])
-    6.0
+    8.0
     >>> average_of_squares([1, 2, 4], [1, 0.5])
     Traceback (most recent call last):
     AssertionError: weights and numbers must have same length
@@ -21,9 +21,9 @@ def average_of_squares(list_of_numbers, list_of_weights=None):
     if list_of_weights is not None:
         assert len(list_of_weights) == len(list_of_numbers), \
             "weights and numbers must have same length"
-        effective_weights = list_of_weights
+        effective_weights = [i/(sum(list_of_weights)) for i in list_of_weights]
     else:
-        effective_weights = [1] * len(list_of_numbers)
+        effective_weights = [1.0/len(list_of_numbers)] * len(list_of_numbers)
     squares = [
         weight * number * number
         for number, weight
@@ -38,7 +38,7 @@ def convert_numbers(list_of_strings):
     Example:
     --------
     >>> convert_numbers(["4", " 8 ", "15 16", " 23    42 "])
-    [4, 8, 15, 16]
+    [4.0, 8.0, 15.0, 16.0, 23.0, 42.0]
 
     """
     all_numbers = []
@@ -60,3 +60,5 @@ if __name__ == "__main__":
     result = average_of_squares(numbers, weights)
     
     print(result)
+    print(numbers)
+    print(weights)

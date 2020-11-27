33ecba581d061447468345e18923268b335d1ea7
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2020-11-27 10:28:22 +0000

    Answers #140

--- week06/average-squares-example/average_squares/squares.py
+++ week06/average-squares-example/average_squares/squares.py
@@ -1,5 +1,5 @@
 """Computation of weighted average of squares."""
-
+from argparse import ArgumentParser
 
 def average_of_squares(list_of_numbers, list_of_weights=None):
     """ Return the weighted average of a list of values.
@@ -51,12 +51,12 @@ def convert_numbers(list_of_strings):
 
 
 if __name__ == "__main__":
-    numbers_strings = ["1","2","4"]
-    weight_strings = ["1","1","1"]        
-    
-    numbers = convert_numbers(numbers_strings)
-    weights = convert_numbers(weight_strings)
-    
-    result = average_of_squares(numbers, weights)
-    
+    parser = ArgumentParser(description="Mean of squared numbers")
+    parser.add_argument("numbers", nargs='+', help="the list of numbers")
+    arguments = parser.parse_args()
+
+    numbers = convert_numbers(arguments.numbers)
+
+    result = average_of_squares(numbers)
+
     print(result)

1fa66251831eaa3c0fb6e8e1bf2e29df6ac1c576
Nikolaos Schoinas <dai16167@uom.edu.gr>
2020-12-09 13:21:04 +0000

    variable named "group" (a dictionary) is passed as an arguments to different functions.

--- week09/refactoring/initial_global.py
+++ week09/refactoring/initial_global.py
@@ -1,16 +1,15 @@
-def average_age():
-    """Compute the average age of the group's members."""
+def average_age(group):
     all_ages = [person["age"] for person in group.values()]
     return sum(all_ages) / len(group)
 
 
-def forget(person1, person2):
+def forget(group, person1, person2):
     """Remove the connection between two people."""
     group[person1]["relations"].pop(person2, None)
     group[person2]["relations"].pop(person1, None)
 
 
-def add_person(name, age, job, relations):
+def add_person(group, name, age, job, relations):
     """Add a new person with the given characteristics to the group."""
     new_person = {
         "age": age,
@@ -20,42 +19,42 @@ def add_person(name, age, job, relations):
     group[name] = new_person
 
 
-group = {
-    "Jill": {
-        "age": 26,
-        "job": "biologist",
-        "relations": {
-            "Zalika": "friend",
-            "John": "partner"
-        }
-    },
-    "Zalika": {
-        "age": 28,
-        "job": "artist",
-        "relations": {
-            "Jill": "friend",
-        }
-    },
-    "John": {
-        "age": 27,
-        "job": "writer",
-        "relations": {
-            "Jill": "partner"
-        }
-    }
-}
-
-nash_relations = {
-    "John": "cousin",
-    "Zalika": "landlord"
-}
-
-add_person("Nash", 34, "chef", nash_relations)
-
-forget("Nash", "John")
 
 if __name__ == "__main__":
+
+    group = {
+        "Jill": {
+            "age": 26,
+            "job": "biologist",
+            "relations": {
+                "Zalika": "friend",
+                "John": "partner"
+            }
+        },
+        "Zalika": {
+            "age": 28,
+            "job": "artist",
+            "relations": {
+                "Jill": "friend",
+            }
+        },
+        "John": {
+            "age": 27,
+            "job": "writer",
+            "relations": {
+                "Jill": "partner"
+            }
+        }
+    }
+
+    nash_relations = {
+        "John": "cousin",
+        "Zalika": "landlord"
+    }
+
+    add_person(group, "Nash", 34, "chef", nash_relations)
+    forget(group, "Nash", "John")
     assert len(group) == 4, "Group should have 4 members"
-    assert average_age() == 28.75, "Average age of the group is incorrect!"
+    assert average_age(group) == 28.75, "Average age of the group is incorrect!"
     assert len(group["Nash"]["relations"]) == 1, "Nash should only have one relation"
     print("All assertions have passed!")

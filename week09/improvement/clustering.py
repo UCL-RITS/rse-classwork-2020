9e1ee4bc07a2e0c05b51f4a24b528ef94dcf5500
Toni Mlinarevic <toni.mlinarevic.20@ucl.ac.uk>
2020-12-10 13:49:29 +0100

    Made code more readable

--- week09/improvement/clustering.py
+++ week09/improvement/clustering.py
@@ -3,30 +3,31 @@
 from math import *
 from random import *
 
-k=3
-
 lines = open('samples.csv', 'r').readlines()
-ps=[]
-for line in lines: ps.append(tuple(map(float, line.strip().split(','))))
+points = []           # list to hold coordinates of points
+for line in lines:
+  values = map(float, line.strip().split(','))
+  points.append(tuple(values))
 
-m=[ps[randrange(len(ps))], ps[randrange(len(ps))], ps[randrange(len(ps))]]
+k=3
+means = [random.choice(points)] * k
 
-alloc=[None]*len(ps)
+alloc=[None]*len(points)
 n=0
 while n<10:
-  for i in range(len(ps)):
-    p=ps[i]
-    d=[None] * 3
-    d[0]=sqrt((p[0]-m[0][0])**2 + (p[1]-m[0][1])**2)
-    d[1]=sqrt((p[0]-m[1][0])**2 + (p[1]-m[1][1])**2)
-    d[2]=sqrt((p[0]-m[2][0])**2 + (p[1]-m[2][1])**2)
+  for i in range(len(points)):
+    p=points[i]
+    d=[None] * k                                     # distance
+    d[0]=sqrt((p[0]-means[0][0])**2 + (p[1]-means[0][1])**2)
+    d[1]=sqrt((p[0]-means[1][0])**2 + (p[1]-means[1][1])**2)
+    d[2]=sqrt((p[0]-means[2][0])**2 + (p[1]-means[2][1])**2)
     alloc[i]=d.index(min(d))
-  for i in range(3):
-    alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
-    new_mean=(sum([a[0] for a in alloc_ps]) / len(alloc_ps), sum([a[1] for a in alloc_ps]) / len(alloc_ps))
-    m[i]=new_mean
+  for i in range(k):
+    alloc_points=[p for j, p in enumerate(points) if alloc[j] == i]
+    new_mean=(sum([a[0] for a in alloc_points]) / len(alloc_points), sum([a[1] for a in alloc_points]) / len(alloc_points))
+    means[i]=new_mean
   n=n+1
 
 for i in range(3):
-  alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
-  print("Cluster " + str(i) + " is centred at " + str(m[i]) + " and has " + str(len(alloc_ps)) + " points.")
+  alloc_points=[p for j, p in enumerate(points) if alloc[j] == i]
+  print("Cluster " + str(i) + " is centred at " + str(means[i]) + " and has " + str(len(alloc_points)) + " points.")

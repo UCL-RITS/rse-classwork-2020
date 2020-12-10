e26744fe02701b366e22ebae29acb7bcf9b3590a
AlexWilkinsonn <alexander.wilkinson.20@ucl.ac.uk>
2020-12-10 12:41:40 +0000

    added comments

--- week09/improvement/clustering.py
+++ week09/improvement/clustering.py
@@ -6,10 +6,12 @@ from random import randrange
 # PARAMETERS
 k=3 # number clusters
 
+# reading in (x,y) coords of the data points
 lines = open('samples.csv', 'r').readlines()
 ps=[]
-for line in lines: ps.append(tuple(map(float, line.strip().split(','))))
+for line in lines: ps.append(tuple(map(float, line.strip().split(',')))) # list of list of coords
 
+# initialising means, k random points from data
 m=[ps[randrange(len(ps))], ps[randrange(len(ps))], ps[randrange(len(ps))]]
 
 alloc=[None]*len(ps)
@@ -18,16 +20,21 @@ while n<10:
   for i in range(len(ps)):
     p=ps[i]
     d=[None] * k
+    # distances of a point to each cluster
     d[0]=sqrt((p[0]-m[0][0])**2 + (p[1]-m[0][1])**2)
     d[1]=sqrt((p[0]-m[1][0])**2 + (p[1]-m[1][1])**2)
     d[2]=sqrt((p[0]-m[2][0])**2 + (p[1]-m[2][1])**2)
+    # allocate point to nearest cluster
     alloc[i]=d.index(min(d))
   for i in range(k):
+    # get points in cluster i
     alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
+    # mean (x,y) of cluster i
     new_mean=(sum([a[0] for a in alloc_ps]) / len(alloc_ps), sum([a[1] for a in alloc_ps]) / len(alloc_ps))
     m[i]=new_mean
   n=n+1
 
+# print results
 for i in range(k):
   alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
   print("Cluster " + str(i) + " is centred at " + str(m[i]) + " and has " + str(len(alloc_ps)) + " points.")

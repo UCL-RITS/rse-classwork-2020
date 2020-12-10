f1f21b9c9a9d0253f0f5867c35dc16c290632ab6
poppynikou <poppyrebecca@hotmail.co.uk>
2020-12-10 12:49:07 +0000

    Changed variable d

--- week09/improvement/trees.py
+++ week09/improvement/trees.py
@@ -13,8 +13,8 @@ for i in range(5):
     nodes=[]
     for j in range(len(d)):
         # works out the location of the next nodes
-        nodes.append([current_node[j][0]+scale_factor*sin(d[j][2]-0.1), current_node[j][1]+scale_factor*cos(current_node[j][2]-0.1), current_node[j][2]-0.1])
-        nodes.append([current_node[j][0]+scale_factor*sin(d[j][2]+0.1), current_node[j][1]+scale_factor*cos(current_node[j][2]+0.1), current_node[j][2]+0.1])
+        nodes.append([current_node[j][0]+scale_factor*sin(current_node[j][2]-0.1), current_node[j][1]+scale_factor*cos(current_node[j][2]-0.1), current_node[j][2]-0.1])
+        nodes.append([current_node[j][0]+scale_factor*sin(current_node[j][2]+0.1), current_node[j][1]+scale_factor*cos(current_node[j][2]+0.1), current_node[j][2]+0.1])
         # plots the branches from previous nodes to next nodes
         plt.plot([current_node[j][0], nodes[-2][0]],[current_node[j][1], nodes[-2][1]])
         plt.plot([current_node[j][0], nodes[-1][0]],[current_node[j][1], nodes[-1][1]])

83f314b1266fb7ad440bc9f2475427075a478e32
AlexWilkinsonn <alexander.wilkinson.20@ucl.ac.uk>
2020-12-10 12:31:50 +0000

    removed magic number, added k param

--- week09/improvement/clustering.py
+++ week09/improvement/clustering.py
@@ -3,7 +3,8 @@
 from math import *
 from random import *
 
-k=3
+# PARAMETERS
+k=3 # number clusters
 
 lines = open('samples.csv', 'r').readlines()
 ps=[]
@@ -16,17 +17,17 @@ n=0
 while n<10:
   for i in range(len(ps)):
     p=ps[i]
-    d=[None] * 3
+    d=[None] * k
     d[0]=sqrt((p[0]-m[0][0])**2 + (p[1]-m[0][1])**2)
     d[1]=sqrt((p[0]-m[1][0])**2 + (p[1]-m[1][1])**2)
     d[2]=sqrt((p[0]-m[2][0])**2 + (p[1]-m[2][1])**2)
     alloc[i]=d.index(min(d))
-  for i in range(3):
+  for i in range(k):
     alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
     new_mean=(sum([a[0] for a in alloc_ps]) / len(alloc_ps), sum([a[1] for a in alloc_ps]) / len(alloc_ps))
     m[i]=new_mean
   n=n+1
 
-for i in range(3):
+for i in range(k):
   alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
   print("Cluster " + str(i) + " is centred at " + str(m[i]) + " and has " + str(len(alloc_ps)) + " points.")

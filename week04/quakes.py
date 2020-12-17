27df785bfd935d217a1fbb8349371611a3e8700c
Cerys <ucapcb0@ucl.ac.uk>
2020-12-17 18:19:46 +0000

    code to find largest earthquake is added

--- week04/quakes.py
+++ week04/quakes.py
@@ -1,19 +1,35 @@
 """A script to find the biggest earthquake in an online dataset."""
 
-# At the top of the file, import any libraries you will use.
-# import ...
+import json
+import requests
 
-# If you want, you can define some functions to help organise your code.
-# def helper_function(argument_1, argument_2):
-#   ...
+quakes = requests.get("http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
+                      params={
+                          'starttime': "2000-01-01",
+                          "maxlatitude": "58.723",
+                          "minlatitude": "50.008",
+                          "maxlongitude": "1.67",
+                          "minlongitude": "-9.756",
+                          "minmagnitude": "1",
+                          "endtime": "2018-10-11",
+                          "orderby": "time-asc"}
+                      )
 
-# When you run the file, it should print out the location and magnitude
-# of the biggest earthquake.
-# You can run the file with `python quakes.py` from this directory.
-if __name__ == "__main__":
-    # ...do things here to find the results...
 
-    # The lines below assume that the results are stored in variables
-    # named max_magnitude and coords, but you can change that.
-    print(f"The maximum magnitude is {max_magnitude} "
-          f"and it occured at coordinates {coords}.")
+
+earthquakesjson=json.loads(quakes.text)
+
+listofquakes=earthquakesjson['features']
+
+largestquake = listofquakes[0] #says the largest quake we have seen so far is the first one
+for i in listofquakes:
+    if i['properties']['mag'] > largestquake['properties']['mag']:
+        largestquake=i
+
+
+lmag=largestquake['properties']['mag']
+llat=largestquake['geometry']['coordinates'][1]
+llong=largestquake['geometry']['coordinates'][0]
+coords=[llat,llong]
+
+print(f"The maximum magnitude is {lmag} " ,f"and it occured at coordinates {coords}.")

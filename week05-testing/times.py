19886b934a66aa52c4c5fd2d1a95da53ddcd97ea
Jasper <ucappji@ucl.ac.uk>
2020-12-23 16:17:55 +0800

    add three further tests

--- week05-testing/times.py
+++ week05-testing/times.py
@@ -15,7 +15,11 @@ def compute_overlap_time(range1, range2):
     overlap_time = []
     for start1, end1 in range1:
         for start2, end2 in range2:
-            low = max(start1, start2)
-            high = min(end1, end2)
-            overlap_time.append((low, high))
+            # both ranges need to start before the other ends, otherwise there is no overlap!
+            if start1 <= end2 and start2 <= end1: 
+                low = max(start1, start2)
+                high = min(end1, end2)
+                if high == low:
+                    continue
+                overlap_time.append((low, high))
     return overlap_time

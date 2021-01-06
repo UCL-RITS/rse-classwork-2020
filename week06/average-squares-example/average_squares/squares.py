c04791f901991c1b5f3992eed651d764a1ddfa1f
Joanna <rmapjs1@ucl.ac.uk>
2021-01-06 22:06:39 +0100

    Fixed the documentation and code for UCL issue #100

--- week06/average-squares-example/average_squares/squares.py
+++ week06/average-squares-example/average_squares/squares.py
@@ -29,7 +29,8 @@ def average_of_squares(list_of_numbers, list_of_weights=None):
         for number, weight
         in zip(list_of_numbers, effective_weights)
     ]
-    return sum(squares)
+    return sum(squares)/(len(list_of_numbers))
+
 
 
 def convert_numbers(list_of_strings):
@@ -38,7 +39,7 @@ def convert_numbers(list_of_strings):
     Example:
     --------
     >>> convert_numbers(["4", " 8 ", "15 16", " 23    42 "])
-    [4, 8, 15, 16]
+    [4.0, 8.0, 15.0, 16.0, 23.0, 42.0]
 
     """
     all_numbers = []
@@ -51,8 +52,8 @@ def convert_numbers(list_of_strings):
 
 
 if __name__ == "__main__":
-    numbers_strings = ["1","2","4"]
-    weight_strings = ["1","1","1"]        
+    numbers_strings = ["2","4"]
+    weight_strings = ["1","0.5"]        
     
     numbers = convert_numbers(numbers_strings)
     weights = convert_numbers(weight_strings)
